dbb307fa2e568b436200c634d41f9a44042ed875
Guilherme Rocha <guilhermefreitasdarocha@gmail.com>
2023-07-13 18:44:55 -0300

    properties, Game, ORM

--- src/main/java/com/project/dslist/DslistApplication.java
+++ src/main/java/com/project/dslist/DslistApplication.java
@@ -1,4 +1,4 @@
-package com.dslist.project;
+package com.project.dslist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/project/dslist/DslistApplicationTests.java
+++ src/test/java/com/project/dslist/DslistApplicationTests.java
@@ -1,4 +1,4 @@
-package com.dslist.project;
+package com.project.dslist;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
