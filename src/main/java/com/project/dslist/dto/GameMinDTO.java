b5f4fd74ed6bd3e5bc7ae29f5ac5d8dabb8149ed
Guilherme Rocha <guilhermefreitasdarocha@gmail.com>
2023-07-17 13:59:23 -0300

    Lista de games

--- src/main/java/com/project/dslist/dto/GameMinDTO.java
+++ src/main/java/com/project/dslist/dto/GameMinDTO.java
@@ -1,32 +1,38 @@
 package com.project.dslist.dto;
 
 import com.project.dslist.entities.Game;
+import com.project.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
     private Long id;
-    private String tittle;
+    private String title;
     private Integer year;
     private String imgUrl;
     private String shortDescription;
 
-    public GameMinDTO (){
+    public GameMinDTO(Game entity) {
+        id = entity.getId();
+        title = entity.getTitle();
+        year = entity.getYear();
+        imgUrl = entity.getImgUrl();
+        shortDescription = entity.getShortDescription();
     }
 
-    public GameMinDTO(Game entity) {
-        this.id = entity.getId();
-        this.tittle = entity.getTitle();
-        this.year = entity.getYear();
-        this.imgUrl = entity.getImgUrl();
-        this.shortDescription = entity.getShortDescription();
+    public GameMinDTO(GameMinProjection projection) {
+        id = projection.getId();
+        title = projection.getTitle();
+        year = projection.getGameYear();
+        imgUrl = projection.getImgUrl();
+        shortDescription = projection.getShortDescription();
     }
 
     public Long getId() {
         return id;
     }
 
-    public String getTittle() {
-        return tittle;
+    public String getTitle() {
+        return title;
     }
 
     public Integer getYear() {
